9782416965d43a64448f426c808c7188f540f6a2
hitaesh77 <hitaesh777@gmail.com>
2025-11-01 23:51:19 -0400

    dockerfile added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.app.router import router
+from backend.db.database import engine
+from backend.db.models import Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Purdue ECE Faculty Finder API",
